4a3e45f834c72ca8b8fd5f7f1b9c94fd0e815f81
Charlie Stanard <louis.stanard@gmail.com>
2015-10-02 14:12:50 -0600

    Add alternate solutions plus my revised final solution

--- your-order-please/code.js
+++ your-order-please/code.js
@@ -1,4 +1,38 @@
-function order(words){
+// Neal's solution
+function order3(words){
+  var arr = words.split(' ');
+  var compare = function(a, b) {
+      if (Number(a.match(/\d+/)[0]) < Number(b.match(/\d+/)[0])) {
+        return -1;
+        }
+      if (Number(a.match(/\d+/)[0]) > Number(b.match(/\d+/)[0])) {
+          return 1;
+      }
+
+      // a must be equal to b
+      return 0;
+  }
+  arr.sort(compare);
+  return arr;
+}
+
+
+// SO user solution
+// http://stackoverflow.com/questions/32910167/javascript-while-loop-efficiency-performance
+function order2(words){
+  return words
+    .split(' ')
+    .sort(function( wordA, wordB ) {
+      var numA = +(wordA.match(/\d+/g)[0]);
+      var numB = +(wordB.match(/\d+/g)[0]);
+      return numA - numB;
+    })
+    .join(' ');
+}
+
+
+// My first attempt - not good!
+function order1(words){
   var ordered = [];
   var arr = words.split(' ');
   var n = 1;
@@ -16,6 +50,21 @@ function order(words){
   }
 
   return ordered.join(' ');
+}
+
+
+// My revised solution
+function order(words) {
+	var arr = words.split(' ');
+	arr.sort(function(a, b) {
+		if (Number(a.match(/\d+/)[0]) < Number(b.match(/\d+/)[0]))
+			return -1;
+		else if (Number(a.match(/\d+/)[0]) > Number(b.match(/\d+/)[0]))
+			return 1
+		else
+			return 0
+	});
+	return arr.join(' ');
 }
 
 console.log(order("is2 Thi1s T4est 3a")); // "Thi1s is2 3a T4est"
